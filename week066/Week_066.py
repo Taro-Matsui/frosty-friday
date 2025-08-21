47bcbd850b729b11c12c135adc97d90faaa83009
Taro-Matsui <Taro.Matsui@ccc.co.jp>
2025-08-21 18:56:11 +0900

    Update Week_066.py

--- week066/Week_066.py
+++ week066/Week_066.py
@@ -1,20 +1,15 @@
-from snowflake.snowpark.functions import col, count
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
 
-def main(session):
+def main(session: snowpark.Session):
     df_tables = session.table("SNOWFLAKE.ACCOUNT_USAGE.TABLES")
     df_filtered = df_tables.filter(
-        (col("IS_TRANSIENT") == "NO") &
-        (col("DELETED").is_null()) &
+        (col("IS_TRANSIENT") == "NO") & 
+        (col("DELETED").is_null()) & 
         (col("ROW_COUNT") > 0)
+    ).select(
+        "TABLE_CATALOG", "TABLE_SCHEMA", "TABLE_NAME", "ROW_COUNT"
     )
-    df_grouped = (
-        df_filtered.group_by(col("TABLE_CATALOG"))
-        .agg(count(col("TABLE_NAME")).alias("TABLES"))
-        .select(
-            col("TABLE_CATALOG").alias("DATABASE"),
-            col("TABLES")
-        )
-        .sort(col("TABLES").desc())
-    )
-    df_grouped.show()
-    return df_grouped
+    df_result = df_filtered.sort(col("ROW_COUNT").desc()).limit(10)  # 行数順に上位10件
+    df_result.show()
+    return df_result
