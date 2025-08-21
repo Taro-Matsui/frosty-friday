a36564cbe2f91bce8a8d2a9ea0318b21b2a8d8ee
Taro-Matsui <Taro.Matsui@ccc.co.jp>
2025-08-21 20:41:06 +0900

    Update Week_066.py

--- week066/Week_066.py
+++ week066/Week_066.py
@@ -2,14 +2,22 @@ import snowflake.snowpark as snowpark
 from snowflake.snowpark.functions import col
 
 def main(session: snowpark.Session):
-    df_tables = session.table("SNOWFLAKE.ACCOUNT_USAGE.TABLES")
-    df_filtered = df_tables.filter(
-        (col("IS_TRANSIENT") == "NO") & 
-        (col("DELETED").is_null()) & 
-        (col("ROW_COUNT") > 0)
-    ).select(
-        "TABLE_CATALOG", "TABLE_SCHEMA", "TABLE_NAME", "ROW_COUNT"
+    df = session.table("SNOWFLAKE.ACCOUNT_USAGE.TABLES")
+    
+    df_result = (
+        df.filter(
+            col("TABLE_CATALOG").isin("SNOWFLAKE", "FROSTY_FRIDAY") &
+            (col("IS_TRANSIENT") == "NO") &
+            (col("DELETED").is_null()) &
+            (col("ROW_COUNT") > 0)
+        )
+        .group_by("TABLE_CATALOG")
+        .count()
+        .select(
+            col("TABLE_CATALOG").alias("DATABASE_NAME"),
+            col("COUNT").alias("TABLE_COUNT")
+        )
+        .sort(col("DATABASE_NAME"))
     )
-    df_result = df_filtered.sort(col("ROW_COUNT").desc()).limit(10)  # 行数順に上位10件
-    df_result.show()
+    
     return df_result
